c3264259f51afc9176b5f0557c12cce1205ae47e
WITCH4242 <76570405+WITCH4242@users.noreply.github.com>
2021-02-12 17:35:16 -0800

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -7,13 +7,14 @@ module.exports = {
     execute(message, args) {
         let update = new Discord.MessageEmbed()
         .setTitle('Developers say')
-        .setDesciption(`Version 0.3.0 is released!!
+        .setDescription(`Version 0.3.0 is released!!
          -Bot source code was completely rebuilt 
          -Fixed Help command
          -Added commands;
           _Wrps_ - _Weightball_ - _Wms_ - _Wping_
          -Improved API and general Latency
          -Made commands handlers shorter and more easy to execute `)
+         .addField(`This is our biggest update so far and we are happy to introduce it. Welcome a new and highly improved Witchery version 0.3.0!!!`)
          .setColor("#D21F3C")
          .setTimestamp()
          .setFooter(`We hope you enjoy the update!`);
