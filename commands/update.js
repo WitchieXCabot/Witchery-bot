c5d32d65a98d6091fb1234847603611118967a11
WITCH4242 <76570405+WITCH4242@users.noreply.github.com>
2021-02-12 17:41:16 -0800

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -14,7 +14,7 @@ module.exports = {
           _Wrps_ - _Weightball_ - _Wms_ - _Wping_
          -Improved API and general Latency
          -Made commands handlers shorter and more easy to execute `)
-         .addField(`This is our biggest update so far and we are happy to introduce it. Welcome a new and highly improved Witchery version 0.3.0!!!`)
+         .addField(`Info``This is our biggest update so far and we are happy to introduce it. Welcome a new and highly improved Witchery version 0.3.0!!!`)
          .setColor("#D21F3C")
          .setTimestamp()
          .setFooter(`We hope you enjoy the update!`, message.author.displayAvatarURL({ format: "png", dynamic: true }));
