53be7fad0229529fd75e101ec653d2d01d872063
WITCH4242 <76570405+WITCH4242@users.noreply.github.com>
2021-04-04 00:33:56 -0700

    Update animal.js

--- commands/animal.js
+++ commands/animal.js
@@ -19,47 +19,42 @@ module.exports = {
                 .setTitle('dog')
                 .setDescription('DOggggg')
 
-                await message.channel.send(dog);
-                message.react('🐶');
+                await message.channel.send(dog).then( sent => {sent.react('🐶')});
             }
             else if (rando == 3){
                 let bird = new Discord.MessageEmbed()
                 .setTitle('bird')
                 .setDescription('Birddddddd')
                 
-                await message.channel.send(bird);
-                message.react('🐦');
+                await message.channel.send(bird).then( sent => {sent.react('🐦')});
             }
             else if (rando == 4){
                 let cat = new Discord.MessageEmbed()
                 .setTitle('cat')
                 .setDescription('Cattttt')
                 
-                await message.channel.send(cat);
-                message.react('🐱');
+                await message.channel.send(cat).then( sent => {sent.react('🐱')});
             }
             else if (rando == 5){
                 let bear = new Discord.MessageEmbed()
                 .setTitle('bear')
                 .setDescription('BEarrrrrr')
                 
-                await message.channel.send(bear);
-                message.react('🐻');
+                await message.channel.send(bear).then( sent => {sent.react('🐻')});
             }
             else if (rando == 6){
                 let worm = new Discord.MessageEmbed()
                 .setTitle('worm')
                 .setDescription('Wormmmmm')
                 
-                await message.channel.send(worm);
-                message.react('828163408246341662');
+                await message.channel.send(worm).then( sent => {sent.react('🌈')});
             }
             else {
                 let zebra = new Discord.MessageEmbed()
                 .setTitle('zebra')
                 .setDescription('Zebraaaaa')
                 
-                await message.channel.send(zebra);
+                await message.channel.send(zebra).then( sent => {sent.react('🦓')});
                 message.react('🦓');
             }
     }
