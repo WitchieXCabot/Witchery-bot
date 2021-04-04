89bee1e802c79f1bdb58f8402a0429f9287f0a6b
WITCH4242 <76570405+WITCH4242@users.noreply.github.com>
2021-04-03 23:51:04 -0700

    Update animal.js

--- commands/animal.js
+++ commands/animal.js
@@ -6,8 +6,8 @@ module.exports = {
     description: "this is send you a random animal with a pic!",
     execute(message, args) {
         var animals = [
-            "Panda",
-            "dog",
+            "Panda '<828159156782891038>'",
+            "dog '<489818863341535247>'",
             "bird",
             "cat",
             "bear",
