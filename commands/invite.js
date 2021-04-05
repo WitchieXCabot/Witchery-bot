1ec05f968a971bcac4edaf939da585c7b47c6323
WITCH4242 <76570405+WITCH4242@users.noreply.github.com>
2021-04-04 21:30:25 -0700

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -5,6 +5,6 @@ module.exports = {
     name: 'invite',
     description: "this is to invite us to your server",
     execute(message, args) {
-            message.channel.send('My invite URL is: ```https://discord.com/oauth2/authorize?client_id=780341354379083797&scope=bot&permissions=8```');
+            message.channel.send('My invite URL is:  https://discord.com/oauth2/authorize?client_id=780341354379083797&scope=bot&permissions=4027055217');
     }
 }
