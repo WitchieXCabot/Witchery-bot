b021b88dc4cfb88411c9a769a4a04f42d7e24077
WITCH4242 <76570405+WITCH4242@users.noreply.github.com>
2021-05-10 09:46:06 -0700

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -6,24 +6,33 @@ module.exports = {
     description: "this is a update embed",
     execute(message, args) {
         let update = new Discord.MessageEmbed()
-        .setTitle('Developers say')
-        .setDescription(`Version 0.3.3 is released!!
-         -Better divination 
-            -commands added
-                -**Woracle**
-                -**W(horoscope)** Ex, Wgemini 
-         -Improved API and general Latency
-         -Help and witchy commands changes
-            -**Woracle-info** added
-            -Removal of member thumbnails in embeds
-            -Smaller help embed
-                -Removed moderation command list
-                (All moderation commands are still on and usabel they just wont show on help)
-        -Website and other source updates
-            All websites have been updated with current info`)
+        .setTitle('Developers say!')
+        .setDescription(`---------------------------------------------
+        Updates:
+            -Woracle is now mobile friendly
+            -Woracle and Whorosope commands have been 
+             edited for better user interaction.
+            -Website and other sources have been 
+             updated; witcherybot.xyz
+        Coming ups:
+            -Say goodbye to help command! We are 
+             updated the help command to send in dm's 
+             since the embed is quite big. We are 
+             also changing some things up in the 
+             embed so expect changes soon!
+            -Extended divination
+             We are adding a three card spread for 
+             Woracle and We are adding a 3-5 rune 
+             reading command. Bump up your divination 
+             with witchery!
+            -Did someone say Extended divination!? We  
+             are adding a Fortune cookie command
+            -We are just getting started expanding! 
+             Expect a lot more to come!`)
          .setColor("#D21F3C")
          .setTimestamp()
-         .setFooter(`We hope you enjoy the update!`, message.author.displayAvatarURL({ format: "png", dynamic: true }));
+         .setFooter(`Witchery©️ - 2021 Copyright©️
+         Witchery Wonders™️ - 2021 LLC`, message.author.displayAvatarURL({ format: "png", dynamic: true }));
         message.channel.send(update);
     }
 }
