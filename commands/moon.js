7308b5c6c03e55878ba97e8e8038bc082497fd2a
WITCH4242 <76570405+WITCH4242@users.noreply.github.com>
2021-05-13 09:56:59 -0700

    Update moon.js

--- commands/moon.js
+++ commands/moon.js
@@ -5,7 +5,7 @@ module.exports = {
     name: 'moon',
     description: "this is a next moon",
     execute(message, args) {
-        message.channel.send(`The next new moon is on;
+        message.channel.send(`The next full moon is on;
         :full_moon: May 26 (Flower Moon in Sagittarius)
         **SUPERMOON LUNAR ECLIPSE!**`)
     }
