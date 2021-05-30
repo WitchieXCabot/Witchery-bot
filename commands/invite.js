ef76e8d270222e25e9fe9c0a9e5942ddb1127c94
WITCH4242 <76570405+WITCH4242@users.noreply.github.com>
2021-05-30 13:21:55 -0700

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -12,13 +12,14 @@ module.exports = {
         .setDescription('\n`\`\`diff\n-WARNING: Something went wrong!\`\`\`')
 
         let links = new Discord.MessageEmbed()
+        .setDescription(' My invite URL is:  \`\`\` https://discord.com/oauth2/authorize?client_id=780341354379083797&scope=bot&permissions=4027055217 \`\`\` ')
         .addField(`\u200B`,`[Invite me](https://discord.com/oauth2/authorize?client_id=780341354379083797&scope=bot&permissions=4027055217) | [Vote](https://top.gg/bot/780341354379083797) | [Website](http://www.witcherybot.xyz/) | [Support Server](https://discord.gg/SR8JanRqsv)`)
         .setColor("#D21F3C")
 
 
 
         try{
-           await message.channel.send(`My invite URL is:  \`\`\` https://discord.com/oauth2/authorize?client_id=780341354379083797&scope=bot&permissions=4027055217 \`\`\` ${links}`);
+           await message.channel.send(links);
         } catch(error) {
             message.reply(err)
         }
