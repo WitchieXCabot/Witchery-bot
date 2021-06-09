4bdbbdd70de4dd3c5d31200efb8c4227f85306b9
WITCH4242 <76570405+WITCH4242@users.noreply.github.com>
2021-06-09 00:13:37 -0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -6,11 +6,11 @@ module.exports = {
     name: 'ping',
     description: "this is send you the uptime of the bot!",
     execute(message, args) { 
-      message.channel.send("Pinging...").then(m =>{
+      message.channel.send(`**Pong!**`).then(m =>{
           var ping = m.createdTimestamp - message.createdTimestamp;
           var yourping = new Date().getTime() - message.createdTimestamp
           
-          m.edit( `**Pong!**` + `\n` + `\n Your ping is ${ping}` + `\n` + `Api latency: ${yourping}` )
+          m.edit(`Your ping is ${ping}` + `\n` + `Api latency: ${yourping}` )
       });
   }
 }
