81fe386298635c42934dc390cc0d72476a5c3df7
WITCH4242 <76570405+WITCH4242@users.noreply.github.com>
2021-06-08 23:54:40 -0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -5,16 +5,18 @@ module.exports = {
 
     name: 'ping',
     description: "this is send you the uptime of the bot!",
-    execute: async (message, args) => { 
-      var ping = message.createdTimestamp - message.createdTimestamp;
-      var yourping = new Date().getTime() - message.createdTimestamp
+    execute(message, args) { 
+      message.channel.send("Pinging...").then(m =>{
+          var ping = m.createdTimestamp - message.createdTimestamp;
+          var yourping = new Date().getTime() - message.createdTimestamp
 
-      var embed = new Discord.MessageEmbed()
-      .setAuthor(`Pong!`)
-      .setTitle(`Your ping is ${ping}`)
-      .setDescription(`Api latency: ${yourping}`)
-      .setColor("#D21F3C")
-      
-      await message.channel.send(embed)
+          var embed = new Discord.MessageEmbed()
+          .setAuthor("Pong!")
+          .setTitle(`Your ping is ${ping}`)
+          .addField(`Api latency: ${yourping}`, `Looks alright :)`)
+          .setColor("#D21F3C")
+          
+          m.edit(embed)
+      });
   }
 }
