d6f3053ed02cf3f65c3087fd60ffdb2042c61a6f
WITCH4242 <76570405+WITCH4242@users.noreply.github.com>
2021-07-15 18:51:58 -0700

    Update tarot.js

--- commands/tarot.js
+++ commands/tarot.js
@@ -10,7 +10,7 @@ module.exports = {
         .setTitle(`Uh oh! :/`)
         .setColor("#D21F3C")
         .setDescription(`This command is no longer avaliable!\nWe had complications but dont worry this command and more will be out in public on **07/19/2021**`)
-        .setFooter(`sorry the hold`)
+        .setFooter(`sorry about the hold :(`)
         message.channel.send(tarot);
 
     }
