3d84597eb36c41d764d58eac22f3ab609834013e
WITCH4242 <76570405+WITCH4242@users.noreply.github.com>
2021-07-19 09:16:34 -0700

    Update shard.js

--- shard.js
+++ shard.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const manager = new Discord.ShardingManager('./index.js', {
   TOKEN: "NzgwMzQxMzU0Mzc5MDgzNzk3.X7trhA.iVuTbhwlRt1jagKgb_d61NGnk5E",
  //autoSpawn: true,
-  totalShards: auto
+  totalShards: 'auto'
 });
 
 manager.on('shardCreate', shard => console.log(`Launching Shard ${shard.id + 1}`, { label: `Shard` }));
